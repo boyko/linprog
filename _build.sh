f97334caa82d40740929a219b3b4d1625c42f0cd
amarov <boyko.amarov@gmail.com>
2021-10-14 23:52:40 +0300

    Move _book to docs.

--- _build.sh
+++ _build.sh
@@ -5,6 +5,7 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+mv _book docs
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
